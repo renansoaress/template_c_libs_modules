11777fa3b30938a99b67e535ddb411ac58bd4214
Renan S <renansoares@live.com>
2024-07-09 00:05:05 -0300

    3rdparty - 1

--- src/main.c
+++ src/main.c
@@ -1,9 +1,21 @@
 #include <stdio.h>
+#include <string.h>
 #include "test.h"
 #include "lib/mylib/mylib.h"
 #include "lib/mylib2/mylib2.h"
 #include "lib/legal/legal.h"
 #include "modules/oi/m_oi.h"
+
+#include "mbedtls/sha256.h"
+
+void print_hash(unsigned char hash[32])
+{
+    for (int i = 0; i < 32; i++)
+    {
+        printf("%02x", hash[i]);
+    }
+    printf("\n");
+}
 
 int main()
 {
@@ -18,5 +30,25 @@ int main()
     teste();
     testesMyLIB();
     auto_lib();
+
+    const char *input = "Hello, world!";
+    unsigned char hash[32];
+
+    // Inicializa o contexto SHA-256
+    mbedtls_sha256_context ctx;
+    mbedtls_sha256_init(&ctx);
+
+    // Inicia o processo de hashing
+    mbedtls_sha256_starts(&ctx, 0); // 0 para SHA-256, 1 para SHA-224
+    mbedtls_sha256_update(&ctx, (unsigned char *)input, strlen(input));
+    mbedtls_sha256_finish(&ctx, hash);
+
+    // Limpa o contexto
+    mbedtls_sha256_free(&ctx);
+
+    // Imprime o hash
+    printf("SHA-256 hash: ");
+    print_hash(hash);
+
     return 0;
 }
